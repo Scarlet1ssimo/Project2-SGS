24b22864ddc717be4005d6ace67301893ee75232
Scarlet_Climax <940012978@qq.com>
2018-10-27 04:21:39 +0800

    Wrote main.c and Changelog.md

---
+++ Changelog.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## 1.0.0
+
+10.26
+
+Basic function without a linked list.
---
+++ main.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#define MAXN 50
+typedef struct Cards
+{
+	int n,a[MAXN];
+}Deck;
+typedef struct Plyr
+{
+	int Nb;							//Number
+	int hlt,hltLv;					//Health point;Health Level
+	int ID;							//Identity: 0:general 1:mercenary
+	int ctry;						//Counttry
+	int Rvld;						//Revealed 0:Unrevealed 1:Revealed
+	Deck Hnd,Eqp,Ftz;				//Hand Zone;Equipment Zone;Fate Zone
+	struct Plyr *Nxt;				//Linked List
+}Player;
+typedef struct Gm
+{
+	Deck Main;						//Main deck
+	int CTRY;						//Main Country
+	int N;							//Live Players
+
+}Game;
+void fateState(Player X)
+{
+	
+}
+void drawState(Player X)
+{
+	X.Hnd.n+=2;
+}
+void playState(Player X)
+{
+
+}
+void discardState(Player X)
+{
+	X.Hnd.n=min(X.Hnd.n,X.X.hltLv);
+}
+int Terminal(Player X)
+{
+	int A[MAXN],tot=1;
+	for(Player Y=X;Y.Nb!=X.Nb;Y=Y.Nxt,tot++)
+	{
+		A[tot]=Y.ID;
+		if(A[tot].Rvld==0)return 0;
+	}
+	if(tot==1)return 1;
+
+}
+void initGame(Game G)
+{
+	
+}
+void initPlayers(int n,Player Head)
+{
+	
+}
+void Perform()
+{
+	Game G;
+	initGame(5,G);
+	Player Head,Now;
+	initPlayers(Head);
+	for(Now=Head;!Terminal();Now=Now.nxt)
+	{
+		fateState(Now);
+		drawState(Now);
+		playState(Now);
+		discardState(Now);
+	}
+
+}
+int main()
+{
+	//Perform();
+	return 0;
+}
