43266abec21fbcb34ca3942ae25e26551cfcc346
Scarlet_Climax <940012978@qq.com>
2018-10-30 03:47:35 +0800

    add main deck

--- Changelog.md
+++ Changelog.md
@@ -5,3 +5,10 @@
 10.26
 
 Basic function without a linked list.
+
+## 1.1.0
+
+10.30
+
+Created the main deck!
+
--- main.c
+++ main.c
@@ -1,14 +1,47 @@
 #include <stdio.h>
-#define MAXN 50
-typedef struct Cards
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define MAXN 120
+#define DEBUG
+/*
+Cards:					type:
+
+Strike					1
+Dodge					2
+Peach					3
+Wine					4
+
+Duel					5
+Dismantle				6
+Snatch					7
+Borrowed sword			8
+
+Arrow barrage			9
+Barbarian invasion		10
+Peach garden			11
+Something for nothing	12
+Bountiful harvest		13
+
+Lighting				14
+Drown in happiness		15
+Starvation				16
+*/
+typedef struct _Card
 {
-	int n, a[MAXN];
+	int num, col;
+	int type;
+} Card;
+typedef struct _Cards
+{
+	int n;
+	Card a[MAXN];
 } Deck;
 typedef struct Plyr
 {
 	int Nb;				//Number
 	int hlt, hltLv;		//Health point;Health Level
-	int ID;				//Identity: 0:general 1:mercenary
+	int ID;				//Identity: 2:general 1:mercenary
 	int ctry;			//Counttry
 	int Rvld;			//Revealed 0:Unrevealed 1:Revealed
 	Deck Hnd, Eqp, Ftz; //Hand Zone;Equipment Zone;Fate Zone
@@ -19,62 +52,187 @@ typedef struct Gm
 	Deck Main; //Main deck
 	int CTRY;  //Main Country
 	int N;	 //Live Players
-
 } Game;
-void fateState(Player X)
+void fateState(Player *X, Game *G);
+void drawState(Player *X, Game *G);
+void playState(Player *X, Game *G);
+void discardState(Player *X, Game *G);
+int Terminal(Player *X);
+void createDeck(Deck *D, int argc, char *file);
+void clearDeck(Deck *D);
+Game *initGame(int n, int argc, char *file);
+void initPlayer(Player *P, int i,Game *G);
+Player *initPlayers(int n,Game *G);
+void Perform(Game *G, Player *Head);
+int rd(int L,int R){return rand() % (R - L + 1) + L;}
+int min(int a, int b);
+
+int main(int argc, char **argv)
 {
+	freopen("err.log", "w", stderr);
+	srand(time(NULL));
+	int n = 5;
+	Game *G = initGame(n, argc, argv[2]);
+	Player *Head = initPlayers(n,G);
+	Perform(G, Head);
+	return 0;
 }
-void drawState(Player X)
+
+void fateState(Player *X, Game *G)
 {
-	X.Hnd.n += 2;
+	for (int i = 0; i < X->Ftz.n; i++);
 }
-void playState(Player X)
+void DRAW(Deck *D,Deck *G){D->a[D->n++] = G->a[--G->n];}
+void drawState(Player *X, Game *G)
 {
-	
+	DRAW(&X->Hnd, &G->Main);
+	DRAW(&X->Hnd, &G->Main);
 }
-int min(int a, int b)
+void playState(Player *X, Game *G)
 {
-	return a < b ? a : b;
+
 }
-void discardState(Player X)
+void discardState(Player *X, Game *G)
 {
-	X.Hnd.n = min(X.Hnd.n,X.hltLv);
+	X->Hnd.n = min(X->Hnd.n, X->hltLv);
 }
-int Terminal(Player X)
+int Terminal(Player *X)
 {
 	int A[MAXN], tot = 1;
-	for (Player Y = X; Y.Nb != X.Nb; Y = *Y.Nxt, tot++)
+	for (Player *Y = X; Y->Nb != X->Nb; Y = Y->Nxt, tot++)
 	{
-		A[tot] = Y.ID;
-		if (Y.Rvld == 0)
+		A[tot] = Y->ID;
+		if (Y->Rvld == 0)
 			return 0;
 	}
 	if (tot == 1)
 		return 1;
+	for (int i = 1; i < tot;i++)
+		if(A[i]!=A[1])
+			return 0;
+	return 1;
 }
-void initGame(Game G)
+void shuffle(Deck *D)
 {
-}
-void initPlayers(int n, Player Head)
-{
-
-}
-void Perform()
-{
-	Game G;
-	initGame(G);
-	Player Head, Now;
-	initPlayers(5, Head);
-	for (Now = Head; !Terminal(Now); Now = *Now.Nxt)
+	for (int i = 1; i < D->n;i++)
 	{
-		fateState(Now);
-		drawState(Now);
-		playState(Now);
-		discardState(Now);
+		int j = rd(0, i);
+		Card t = D->a[i];
+		D->a[i] = D->a[j];
+		D->a[j] = t;
 	}
 }
-int main()
+void createDeck(Deck *D, int argc, char *file)
 {
-	//Perform();
-	return 0;
+	clearDeck(D);
+	FILE *fp;
+	if (argc==3)
+		fp = fopen(file, "r");
+	else 
+	#ifdef DEBUG
+		fp = fopen("w.in", "r");
+	#else
+		fp = stdin;
+	#endif
+	char S[MAXN];
+	D->n = 0;
+	while (~fscanf(fp, "%s", S))
+	{
+		int num;
+		fscanf(fp, "%d", &num);
+		for (int i = D->n; i < D->n+ num;i++)
+		{
+			if (!strcmp(S, "Strike"))
+				D->a[i] = (Card){rd(1, 13), rd(1, 4), 1};
+			if (!strcmp(S, "Dodge"))
+				D->a[i] = (Card){rd(1, 13), rd(1, 4), 2};
+			if (!strcmp(S, "Peach"))
+				D->a[i] = (Card){rd(1, 13), rd(1, 4), 3};
+			if (!strcmp(S, "Wine"))
+				D->a[i] = (Card){rd(1, 13), rd(1, 4), 4};
+			if (!strcmp(S, "Dismatle"))
+				D->a[i] = (Card){rd(1, 13), rd(1, 4), 5};
+			if (!strcmp(S, "Duel"))
+				D->a[i] = (Card){rd(1, 13), rd(1, 4), 6};
+			if (!strcmp(S, "Snatch"))
+				D->a[i] = (Card){rd(1, 13), rd(1, 4), 7};
+			if (!strcmp(S, "BorrowedSword"))
+				D->a[i] = (Card){rd(1, 13), rd(1, 4), 8};
+			if (!strcmp(S, "ArrowBarrage"))
+				D->a[i] = (Card){rd(1, 13), rd(1, 4), 9};
+			if (!strcmp(S, "BarbarianInvasion"))
+				D->a[i] = (Card){rd(1, 13), rd(1, 4), 10};
+			if (!strcmp(S, "PeachGarden"))
+				D->a[i] = (Card){rd(1, 13), rd(1, 4), 11};
+			if (!strcmp(S, "SomethingForNothing"))
+				D->a[i] = (Card){rd(1, 13), rd(1, 4), 12};
+			if (!strcmp(S, "BountifulHarvest"))
+				D->a[i] = (Card){rd(1, 13), rd(1, 4), 13};
+			if (!strcmp(S, "Lighting"))
+				D->a[i] = (Card){rd(1, 13), rd(1, 4), 14};
+			if (!strcmp(S, "DrownInHappiness"))
+				D->a[i] = (Card){rd(1, 13), rd(1, 4), 15};
+			if (!strcmp(S, "Starvation"))
+				D->a[i] = (Card){rd(1, 13), rd(1, 4), 16};
+		}
+		D->n += num;
+	}
+	shuffle(D);
+	for (int i = 0; i < D->n;i++)fprintf(stderr, "%d\n", D->a[i].type);
+	fclose(fp);
+}
+void clearDeck(Deck *D)
+{
+	memset(D, 0, sizeof(Deck));
+}
+Game *initGame(int n, int argc, char *file)
+{
+	Game *G = (Game *)malloc(sizeof(Game));
+	G->N = n;
+	createDeck(&G->Main, argc, file);
+	G->CTRY = 0;
+	return G;
+}
+void initPlayer(Player *P, int i,Game *G)
+{
+	P->Nb = i;
+	P->hltLv = rd(3, 5);
+	P->hlt = P->hltLv;
+	P->ID = 0;
+	P->ctry = rd(1, 4);
+	P->Rvld = 0;
+	clearDeck(&P->Hnd);
+	clearDeck(&P->Eqp);
+	clearDeck(&P->Ftz);
+	for (int i = 1; i <= 5;i++)
+		DRAW(&P->Hnd, &G->Main);
+}
+Player *initPlayers(int n,Game *G)
+{
+	Player *Head = (Player *)malloc(sizeof(Player)), *Now = Head;
+	for (int i = 1; i < n; i++)
+	{
+		initPlayer(Now, i,G);
+		Now->Nxt = (Player *)malloc(sizeof(Player));
+		Now = Now->Nxt;
+	}
+	initPlayer(Now, n,G);
+	Now->Nxt = Head;
+	return Head;
+}
+void Perform(Game *G, Player *Head)
+{
+	Player *Now = Head;
+	for (; !Terminal(Now); Now = Now->Nxt)
+	{
+		fateState(Now, G);
+		drawState(Now, G);
+		playState(Now, G);
+		discardState(Now, G);
+	}
+	free(G);
+}
+int min(int a, int b)
+{
+	return a < b ? a : b;
 }
